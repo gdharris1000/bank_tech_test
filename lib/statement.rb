997a6b887db9d12745e03b1176f01fb354200ab3
Gareth Harris <gdharris1000@gmail.com>
2020-04-21 10:37:25 +0100

    statement class

--- lib/statement.rb
+++ lib/statement.rb
@@ -5,7 +5,16 @@ class Statement
     end
 
     def generate
-        header = "date || credit || debit || balance\n"
-        return header
+        statementText = "date || credit || debit || balance\n"
+
+        @history.reverse.each do |h|
+            credit = h.credit != "" ? ('%.2f' % h.credit.to_f) : ""
+            debit = h.debit != "" ? ('%.2f' % h.debit.to_f) : ""
+            statementText += "#{h.date} || #{credit} || #{debit} || #{'%.2f' % h.balance.to_f}\n"
+        end
+        
+        return statementText
     end
+
+
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -3,14 +3,18 @@ require 'statement'
 describe Statement do
 
     before(:each) do
-        transaction1 = instance_double("Transaction", :credit => 10, :debit => "", :balance => 10)
-        transaction2 = instance_double("Transaction", :credit => "", :debit => 1, :balance => 9)
+        transaction1 = instance_double("Transaction", :date => "2020/04/18", :credit => 10, :debit => "", :balance => 10)
+        transaction2 = instance_double("Transaction", :date => "2020/04/20", :credit => "", :debit => 1, :balance => 9)
         history = [transaction1, transaction2]
         @statement = Statement.new(history)
     end
 
-    it 'generateStatement returns header text' do
-        expect(@statement.generate).to eq "date || credit || debit || balance\n"
+    it 'generate returns header text' do
+        statement = Statement.new([])
+        expect(statement.generate).to eq "date || credit || debit || balance\n"
     end
 
+    it 'generate turns history array into a string' do
+        expect(@statement.generate).to eq "date || credit || debit || balance\n2020/04/20 ||  || 1.00 || 9.00\n2020/04/18 || 10.00 ||  || 10.00\n"
+    end
 end
