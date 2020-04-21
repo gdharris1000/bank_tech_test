6656840a97ee541d35ffccf74d00f286134c47e9
Gareth Harris <gdharris1000@gmail.com>
2020-04-21 11:17:40 +0100

    account tests added

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -16,6 +16,21 @@ describe Account do
     @account.instance_variable_set(:@transactionController, controller_double)
     expect{@account.statement}.to output("date || credit || debit || balance\n2020/04/01 || 10.00 ||  || 10.00\n").to_stdout
   end
+  
+  context 'deposit and withdraw' do
 
- 
+    before(:each) do
+      controller_spy = spy("TransactionController")
+    @account.instance_variable_set(:@transactionController, controller_spy)
+    end
+    
+    it 'user can deposit a value' do
+      expect{@account.deposit(10)}.not_to raise_error
+    end
+  
+    it 'user can withdraw a value' do
+      expect{@account.withdraw(10)}.not_to raise_error
+    end
+  end
+
 end
