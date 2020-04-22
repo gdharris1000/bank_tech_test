30d93ed323b6505c754eb95447534e8aec9c3ba7
Gareth Harris <gdharris1000@gmail.com>
2020-04-22 11:43:36 +0100

    refactored to include dependency injection and updated tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,19 +3,23 @@
 require 'account'
 
 describe Account do
-  before(:each) do
-    @account = Account.new
+
+  context 'initialise' do
+    it 'creates a new transaction controller on initialisation' do
+      account = Account.new
+      expect(account.instance_variable_get(:@transactionController)).to be_a TransactionController
+    end
   end
 
-  it 'creates a new transaction controller on initialisation' do
-    expect(@account.instance_variable_get(:@transactionController)).to be_a TransactionController
+  before(:each) do
+    transaction1 = instance_double('Transaction', date: '2020/04/01', credit: 10, debit: '', balance: 10)
+    @controller_double = double('TransactionController', :addToHistory => "added to history", :history => [transaction1])
+    @account = Account.new(@controller_double)
   end
 
   it 'returns a statement' do
-    transaction1 = instance_double('Transaction', date: '2020/04/01', credit: 10, debit: '', balance: 10)
-    controller_double = instance_double('TransactionController', history: [transaction1])
-    @account.instance_variable_set(:@transactionController, controller_double)
-    expect { @account.statement }.to output("date || credit || debit || balance\n2020/04/01 || 10.00 ||  || 10.00\n").to_stdout
+    statement_doube = double('Statement', :generate => "date || credit || debit || balance\n2020/04/01 || 10.00 ||  || 10.00\n")
+    expect { @account.statement( statement_doube) }.to output("date || credit || debit || balance\n2020/04/01 || 10.00 ||  || 10.00\n").to_stdout
   end
 
   context 'deposit and withdraw' do
@@ -25,11 +29,11 @@ describe Account do
     end
 
     it 'user can deposit a value' do
-      expect { @account.deposit(10) }.not_to raise_error
+      expect ( @account.deposit(10) ).to eq "added to history"
     end
 
     it 'user can withdraw a value' do
-      expect { @account.withdraw(10) }.not_to raise_error
+      expect ( @account.withdraw(10) ).to eq "added to history"
     end
   end
 end
